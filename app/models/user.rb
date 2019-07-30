ef69123d4c2c7b500fbfa5c95caf4962974f16dd
Francois Nguyen <francois.nguyen2@gmail.com>
2019-07-30 13:53:04 +0200

    added to model user has many orders and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :orders, dependent: :destroy
+  validates :gender, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :zip_code, presence: true
+  validates :country, presence: true
 end
