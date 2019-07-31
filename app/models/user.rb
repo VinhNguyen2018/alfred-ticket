96d4084e79ebd706e7819e0dc0279abce45841d7
Francois Nguyen <francois.nguyen2@gmail.com>
2019-07-31 18:12:38 +0200

    added some seed css on user sign in and new sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders, dependent: :destroy
   validates :gender, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroy all orders"
+
+Order.destroy_all
+
+puts "destroy all concerts events"
+
+ConcertEvent.destroy_all
+
+puts "destroy all concert halls"
+
+ConcertHall.destroy_all
+
+puts "destroy all users"
+
+User.destroy_all
+
+puts "destroyed all database"
+
+puts "Creating Users"
+
+francois = User.new(
+    first_name: "François",
+    last_name: "Nguyen",
+    gender: "M",
+    address: "16 villa Gaudelet",
+    email: "francois@gmail.com",
+    password: "ahahahah",
+    country: "France",
+    zip_code: "75011",
+    city: "Paris",
+    phone_number: "0629971405"
+  )
+francois.save!
+
+puts "Creating concert halls"
+
+accor_hotel_arena = ConcertHall.new(
+    name: "AccorHotels Arena",
+    address: "8 Boulevard de Bercy, 75012 Paris",
+    country: "France"
+  )
+accor_hotel_arena.save!
+
+puts "Creating concert events"
+
+beyonce = ConcertEvent.new(
+    event_date: "01/01/2020",
+    artist_name: "Beyoncé",
+    photo: "../assets/images/beyonce.jpeg",
+    category: "Catégorie unique",
+    price_cents: 10000,
+    concert_hall: ConcertHall.first
+  )
+
+beyonce.save!
+
+ed_sheeran = ConcertEvent.new(
+    event_date: "01/01/2020",
+    artist_name: "Ed Sheeran",
+    photo: "../assets/images/edsheeran.jpeg",
+    category: "Catégorie unique",
+    price_cents: 5000,
+    concert_hall: ConcertHall.first
+  )
+
+ed_sheeran.save!
+
+puts "finished"
