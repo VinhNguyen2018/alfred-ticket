772f427baaa456b249ce5010fa4848fdc65566f5
Francois Nguyen <francois.nguyen2@gmail.com>
2019-08-01 16:23:15 +0200

    change routes to display in url concerts instead of concert_events and css change on navbar compatible with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :concert_events, only: [:index, :show], as: "concerts"
+  resources :concert_events, only: [:index, :show], as: "concerts", :path => "/concerts"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
