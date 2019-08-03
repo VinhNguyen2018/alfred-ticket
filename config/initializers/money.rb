f5b910d729e6812b7316e81f9c4f890f2f179d83
Francois Nguyen <francois.nguyen2@gmail.com>
2019-08-03 23:59:26 +0200

    displayed categories and prices with symbol after on show

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -8,6 +8,7 @@ MoneyRails.configure do |config|
   #
   config.default_currency = :eur
 
+
   # Set default bank object
   #
   # Example:
@@ -48,17 +49,17 @@ MoneyRails.configure do |config|
   # Register a custom currency
   #
   # Example:
-  # config.register_currency = {
-  #   priority:            1,
-  #   iso_code:            "EU4",
-  #   name:                "Euro with subunit of 4 digits",
-  #   symbol:              "€",
-  #   symbol_first:        true,
-  #   subunit:             "Subcent",
-  #   subunit_to_unit:     10000,
-  #   thousands_separator: ".",
-  #   decimal_mark:        ","
-  # }
+  config.register_currency = {
+    priority:            1,
+    iso_code:            "EU4",
+    name:                "Euro with subunit of 4 digits",
+    symbol:              "€",
+    symbol_first:        false,
+    subunit:             "Subcent",
+    subunit_to_unit:     10000,
+    thousands_separator: " ",
+    decimal_mark:        "."
+  }
 
   # Specify a rounding mode
   # Any one of:
@@ -82,7 +83,7 @@ MoneyRails.configure do |config|
   # config.default_format = {
   #   no_cents_if_whole: nil,
   #   symbol: nil,
-  #   sign_before_symbol: nil
+  #   sign_after_symbol: true
   # }
 
   # Set default raise_error_on_money_parsing option
