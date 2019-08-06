53b0471d03b4212b27ce042d2cf99b38b1c01de8
Francois Nguyen <francois.nguyen2@gmail.com>
2019-08-06 15:34:01 +0200

    added quantity and categories references to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_073933) do
+ActiveRecord::Schema.define(version: 2019_08_06_133152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2019_08_03_073933) do
     t.bigint "concert_event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_orders_on_category_id"
     t.index ["concert_event_id"], name: "index_orders_on_concert_event_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -81,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_073933) do
 
   add_foreign_key "categories", "concert_events"
   add_foreign_key "concert_events", "concert_halls"
+  add_foreign_key "orders", "categories"
   add_foreign_key "orders", "concert_events"
   add_foreign_key "orders", "users"
 end
