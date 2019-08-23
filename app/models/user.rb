5a4ae066e138ef42565f6bafef5dd31660d6e52d
Francois Nguyen <francois.nguyen2@gmail.com>
2019-08-24 00:44:44 +0200

    added sign up with css and new controller registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders, dependent: :destroy
+  has_one :billing, dependent: :destroy
   validates :gender, presence: true
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:update, :create]
   root to: 'pages#home'
   get "dashboard", to: "dashboards#index", as: "dashboard"
