ebb8c049df47803562254f06537a87e6ef66215d
Francois Nguyen <francois.nguyen2@gmail.com>
2019-08-27 00:36:52 +0200

    fix js calculation ttc price

--- app/javascript/components/calculate_TVA_TTC.js
+++ app/javascript/components/calculate_TVA_TTC.js
@@ -1,28 +1,28 @@
 const calculateFastDelivery = () => {
+  const blockVat = document.querySelector(".TVA");
+  const blockTtc = document.querySelector(".TTC");
   const quantity = parseInt(document.getElementById("quantity-order").innerText);
   const price = parseFloat(parseFloat(document.getElementById("unit-price").innerText.replace(',','.')).toFixed(2));
   const managementFees = parseFloat(parseFloat(quantity*price*40/100).toFixed(2));
-  const blockVat = document.querySelector(".TVA");
-  const blockTtc = document.querySelector(".TTC");
   const FastDeliveryFees = 5;
-  const vatAmount = parseFloat((quantity*price+managementFees+FastDeliveryFees)*20/100).toFixed(2);
+  const vatAmount = parseFloat(parseFloat((quantity*price+managementFees+FastDeliveryFees)*20/100).toFixed(2));
   const totalPrice = parseFloat(quantity*price+managementFees+FastDeliveryFees+vatAmount).toFixed(2);
-  const frVatAmount = vatAmount.replace('.',',');
-  const frTotalPrice = totalPrice.replace('.',',');
+  const frVatAmount = (vatAmount + '').replace('.',',');
+  const frTotalPrice = (totalPrice + '').replace('.',',');
   blockVat.innerHTML = frVatAmount;
   blockTtc.innerHTML = frTotalPrice;
 }
 
 const calculateStandardDelivery = () => {
+  const blockVat = document.querySelector(".TVA");
+  const blockTtc = document.querySelector(".TTC");
   const quantity = parseInt(document.getElementById("quantity-order").innerText);
   const price = parseFloat(parseFloat(document.getElementById("unit-price").innerText.replace(',','.')).toFixed(2));
   const managementFees = parseFloat(parseFloat(quantity*price*40/100).toFixed(2));
-  const blockVat = document.querySelector(".TVA");
-  const blockTtc = document.querySelector(".TTC");
-  const vatAmount = parseFloat((quantity*price+managementFees)*20/100).toFixed(2);
+  const vatAmount = parseFloat(parseFloat((quantity*price+managementFees)*20/100).toFixed(2));
   const totalPrice = parseFloat(quantity*price+managementFees+vatAmount).toFixed(2);
-  const frVatAmount = vatAmount.replace('.',',');
-  const frTotalPrice = totalPrice.replace('.',',');
+  const frVatAmount = (vatAmount + '').replace('.',',');
+  const frTotalPrice = (totalPrice + '').replace('.',',');
   blockVat.innerHTML = frVatAmount;
   blockTtc.innerHTML = frTotalPrice;
 }
