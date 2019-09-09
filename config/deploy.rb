29cd5ffe21b6616a21585f15c1c8c9e36dfbe85a
Francois Nguyen <francois.nguyen2@gmail.com>
2019-09-09 10:32:33 +0200

    test changing set rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:VinhNguyen2018/alfred-ticket.git"
 
 set :deploy_to, '/home/ruby-app/www/'
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.3'
+set :rbenv_ruby, `cat .ruby-version`.chomp
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
