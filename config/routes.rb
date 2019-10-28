6a5d03bf701d7bfd9cc93304efe60946b2e07c5a
Francois Nguyen <francois.nguyen2@gmail.com>
2019-10-28 08:27:32 +0100

    added better css on login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # devise_for :users, controllers: { registrations: 'users/registrations',  }
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users, only: [:update, :create]
   root to: 'pages#home'
